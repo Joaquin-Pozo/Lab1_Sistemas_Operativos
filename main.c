e411452fe7f3c0b0170f25154ce9880b4887e39f
Joaquín Pozo <joaquin.pozo@usach.cl>
2025-09-27 17:55:53 -0300

    Implementación de función para contar dígitos en un arreglo de números utilizando procesos hijos con fork().

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 // Funcion que verifica si un caracter es un numero
 int isDigit(char c) {
@@ -10,37 +14,82 @@ int isDigit(char c) {
 }
 
 // Función que lee un archivo y transforma los números de char a int
-void leerArchivo(char nombreArchivo[]) {
+int *leerArchivo(char nombreArchivo[], int *cantidad) {
     FILE *archivo = fopen(nombreArchivo, "r");
 
     // Comprueba que el archivo exista en el mismo path
     if (archivo == NULL) {
         printf("Error al abrir el archivo %s\n", nombreArchivo);
-        return;
+        *cantidad = 0;
+        return NULL;
     }
 
-    int numeros[1000];
-    int i = 0;
+    // Crea un arreglo dinámico para almacenar el contenido del archivo
+    int *arrayNumeros = malloc(1000 * sizeof(int));
+    if (!arrayNumeros) {
+        printf("Error asignando memoria. El archivo es muy largo\n");
+        *cantidad = 0;
+        fclose(archivo);
+        return NULL;
+    }
 
+    int i = 0;
     int c;
+    // lee el archivo hasta que no queden datos
     while ((c = fgetc(archivo)) != EOF) {
         if (isDigit((char)c)) {
-            if (i < (int)(sizeof(numeros) / sizeof(numeros[0]))) {
-                // convierte el numero de char a int
-                numeros[i++] = c - '0'; 
+            if (i < 1000) {
+                // convierte el numero de char a int y lo almacena en el arreglo de enteros
+                arrayNumeros[i++] = c - '0'; 
             }
         }
     }
 
     fclose(archivo);
 
-    // Imprime los números encontrados
-    printf("Numeros: ");
-    for (int j = 0; j < i; j++) {
-        printf("%d ", numeros[j]);
-    }
-    printf("\n");
+    *cantidad = i;
+    return arrayNumeros;
+}
 
+void conteoDigitos(int arrayNumeros[], int cantidadNumeros) {
+    printf("Conteo de dígitos:\n");
+    int sumaDigitos = 0;
+    
+    for (int i = 0; i < 5; i++) {
+        int fd[2]; // arreglo de 2 elementos, [0] para lectura y [1] para escritura
+        pid_t pid_hijo;
+        char buffer[100];
+        char mensaje_hijo[100];
+        int digitos = 0;
+
+        pipe(fd);
+        pid_hijo = fork();
+
+        for (int j = 0; j < cantidadNumeros; j++) {
+            if (arrayNumeros[j] == i*2 || arrayNumeros[j] == i*2 + 1) {
+                digitos++;
+            }
+        }
+        // almacena el mensaje en buffer
+        snprintf(mensaje_hijo, sizeof(mensaje_hijo), "%d-%d: %d", i*2, i*2 + 1, digitos);
+        sumaDigitos = sumaDigitos + digitos;
+
+        if (pid_hijo == 0) {
+            //printf("Soy el hijo: %d, y mi padre es: %d\n", getpid(), getppid());
+            close(fd[0]); // cierra el descriptor de lectura
+            write(fd[1], mensaje_hijo, strlen(mensaje_hijo) + 1); // envia el mensaje al padre
+            close(fd[1]); // cierra el descriptor de escritura
+            exit(EXIT_SUCCESS);
+        }
+        //printf("Soy el proceso padre: %d, mi hijo es: %d\n", getpid(), pid_hijo);
+        close(fd[1]); // para recibir el mensaje, cierra el descriptor de escritura
+        read(fd[0], buffer, sizeof(buffer));
+        printf("%s\n", buffer);
+
+        close(fd[0]);
+    }
+    printf("Total Dígitos: %d\n", sumaDigitos);
+    
 }
 
 int main () {
@@ -56,9 +105,15 @@ int main () {
     // elimina el salto de linea que deja fgets
     str[strcspn(str, "\n")] = '\0';
 
-    printf("El nombre del archivo es: %s\n", str);
+    int cantidadNumeros = 0;
+    int *arrayNumeros = leerArchivo(str, &cantidadNumeros);
 
-    leerArchivo(str);
+    if (arrayNumeros == NULL || cantidadNumeros == 0) {
+        return 0;
+    }
+    conteoDigitos(arrayNumeros, cantidadNumeros);
+
+    free(arrayNumeros);
 
     return 0;
 }
